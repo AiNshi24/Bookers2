572cbcfe49aa861b53fe1e4fda43b97a4b1f4c2f
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-01-22 08:46:28 +0000

    [Add]index画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,16 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
   def show
     @user = User.find(params[:id])
     @books = @user.books
   end
 
   def edit
-    user_id = params[:id].to_i
-    unless user_id == current_user.id
-      redirect_to new_user_session_path
-    end 
     @user = User.find(params[:id])
   end
   
   def update
-    user_id = params[:id].to_i
-    unless user_id == current_user.id
-      redirect_to new_user_session_path
-    end 
-    
-    @eser = User.find(params[:id])
+    @user = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
   end 
@@ -32,5 +24,10 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end 
-  
+  def is_matching_login_user
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to post_images_path
+    end
+  end
 end 
