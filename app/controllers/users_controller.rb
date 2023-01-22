dfa48cd9854d34beb316b0bc03643ed31ce91291
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-01-22 05:47:29 +0000

    [Add]ページレイアウト

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,19 @@ class UsersController < ApplicationController
   end
 
   def edit
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to new_user_session_path
+    end 
     @user = User.find(params[:id])
   end
   
   def update
+    user_id = params[:id].to_i
+    unless user_id == current_user.id
+      redirect_to new_user_session_path
+    end 
+    
     @eser = User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path(@user.id)
@@ -24,4 +33,4 @@ class UsersController < ApplicationController
     params.require(:user).permit(:name, :profile_image, :introduction)
   end 
   
-end
+end 
