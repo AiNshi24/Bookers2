a81e1059d2e0973835918df7a56f900a0f88bf69
AiNshi24 <121652615+AiNshi24@users.noreply.github.com>
2023-01-22 11:57:03 +0000

    [Update]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,5 @@
 class BooksController < ApplicationController
-  def new
-    @book = Book.new
-  end
+  
   
   def create
     @book = Book.new(book_params)
@@ -12,7 +10,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
-    
+    @book = Book.new
   end
 
   def show
